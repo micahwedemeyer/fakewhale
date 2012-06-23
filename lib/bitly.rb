29250b50a9eaea2151d3a76ebabf362cd9317f32
Micah Wedemeyer <me@micahwedemeyer.com>
2012-06-23 10:35:16 -0400

    Fix bitly crash

--- lib/bitly.rb
+++ lib/bitly.rb
@@ -6,6 +6,9 @@ module Bitly
     bitly_url = "http://api.bit.ly/shorten?version=2.0.1&login=#{BITLY_LOGIN}&apiKey=#{BITLY_API_KEY}&longUrl=#{CGI::escape(url)}"
     resp = open(bitly_url).read
     JSON.parse(resp)['results'][url]['shortUrl']
+  rescue
+    logger.debug("Unable to generate Bit.ly url for #{url}")
+    nil
   end
 end
 
